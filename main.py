f00c54df928c1113701e05829364e489593b3af2
nicolasmogni <nicolasmogni@gmail.com>
2025-01-27 23:27:29 +0000

    New functions, count_words and count_characters

--- main.py
+++ main.py
@@ -2,5 +2,28 @@
 def main():
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
-    print(file_contents)
+    num_words = count_words(file_contents)
+    num_characters = count_characters(file_contents)
+    print(f"Total of words: {num_words}")
+    print(num_characters)
+
+
+def count_words(book):
+        words = book.split()
+        return len(words)
+        
+def count_characters(book):
+    characters = {}
+    textLowered = book.lower()
+
+    for i in textLowered:
+        if i in characters:
+            characters[i] += 1
+        else:
+            characters[i] = 1
+
+    return characters
+
+
 main()
+
