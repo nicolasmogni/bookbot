ad9813d052d4c398bd585dee41be9734d4f16650
nicolasmogni <nicolasmogni@gmail.com>
2025-01-28 22:45:24 +0000

    Refactor

--- main.py
+++ main.py
@@ -1,35 +1,39 @@
 
 def main():
-    with open('books/frankenstein.txt') as f:
-        file_contents = f.read()
-    num_words = count_words(file_contents)
-    num_characters = count_characters(file_contents)
-    # print(f'Total of words: {num_words}')
-    # print(num_characters)
-
-    dictionary_list = [{'char': letter, 'num': count} for letter, count in num_characters.items() if letter.isalpha()] 
-    dictionary_list.sort(key=sort_on, reverse=True)
+    book = get_book('books/frankenstein.txt')
+    num_words = get_words_len(book)
+    num_characters = get_count_chars_dict(book)
+    list = chars_dict_to_list(num_characters)
 
     print('***** Report of Frankenstein book *****')
     print(f'Total of words: {num_words}')
-    for i in dictionary_list:
+    print("")
+    for i in list:
         print(f"The '{i['char']}' character was found {i['num']} times")
 
     print('***** End report *****')
 
- 
+def get_book(book_path):
+    with open(book_path) as f:
+        return f.read()
+
+def chars_dict_to_list(dict):
+    dict_list = [{'char': letter, 'num': count} for letter, count in dict.items() if letter.isalpha()]    
+    dict_list.sort(key=sort_on, reverse=True)
+    return dict_list
+
 def sort_on(dict):
     return dict['num']
 
-def count_words(book):
-        words = book.split()
-        return len(words)
+def get_words_len(book):
+    words = book.split()
+    return len(words)
         
-def count_characters(book):
+def get_count_chars_dict(book):
     characters = {}
-    textLowered = book.lower()
+    lowered_book = book.lower()
 
-    for i in textLowered:
+    for i in lowered_book:
         if i in characters:
             characters[i] += 1
         else:
@@ -39,4 +43,3 @@ def count_characters(book):
 
 
 main()
-
